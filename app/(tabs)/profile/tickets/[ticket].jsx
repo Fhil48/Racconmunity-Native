616e6b817e6621969db90cf40f361fd42568a194
Diego San Martin Gonzalez <diegorsk2@gmail.com>
2024-07-03 16:53:39 -0400

    Fix FormDate component

--- app/(tabs)/profile/tickets/[ticket].jsx
+++ app/(tabs)/profile/tickets/[ticket].jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { router, useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ReturnButton from "../../../../components/profile/ReturnButton";
-import { images } from "../../../../constants";
 import { CustomButton } from "../../../../components";
 
 const Ticket = () => {
@@ -19,11 +18,11 @@ const Ticket = () => {
             handlePress={() => router.push("/profile/tickets")}
           />
           <View className=" -z-10 absolute w-[400px] h-[300px]">
-            <Image source={images.empty} className="h-40  w-full" />
+            <Image source={ticket.thumbnail} className="h-40  w-full" />
           </View>
           <View className="mt-[25vh] ml-[35vw] items-center absolute z-20">
             <Image
-              source={images.profile}
+              source={ticket.user.avatar}
               resizeMode="cover"
               className="w-20 h-20 rounded-xl"
             />
@@ -31,26 +30,13 @@ const Ticket = () => {
           <View className="w-full py-4 mb-auto rounded-[40px] rounded-b-none mt-32 bg-[#D9D9D9] z-10">
             <View className="mt-20 ml-4 mb-4">
               <Text className="text-2xl text-primary  font-pbold">
-                Paseo de perros en el Parque Central
+                {ticket.title}
               </Text>
-              <Text className="">Autor Name LastName</Text>
+              <Text className="">{ticket.author.username}</Text>
             </View>
             <View className="mt-2 ml-4">
               <Text className="font-pmedium text-sm text-primary mb-4">
-                ¡Hola, vecinos! ¿Necesitan ayuda para que sus adorables mascotas
-                se mantengan activas y felices? Estoy ofreciendo un servicio de
-                paseo de mascotas para asegurarme de que sus compañeros peludos
-                reciban el ejercicio y el cuidado que necesitan. Ofrezco paseos
-                diarios de 30 minutos a una hora, según las necesidades de su
-                mascota, con atención personalizada, cuidado y seguridad. Al
-                final de cada paseo, recibirán un pequeño informe con fotos y
-                detalles sobre cómo fue el paseo. Amo a los animales y disfruto
-                pasar tiempo con ellos, soy confiable y responsable, y ofrezco
-                horarios flexibles para adaptarme a sus necesidades. Si están
-                interesados en darle a su mascota un paseo divertido y
-                saludable, no duden en contactarme. Estoy aquí para ayudar a que
-                sus amigos de cuatro patas vivan felices y activos. ¡Espero
-                conocer a sus maravillosas mascotas pronto!
+                {ticket.description}
               </Text>
             </View>
             <CustomButton
@@ -66,5 +52,3 @@ const Ticket = () => {
 };
 
 export default Ticket;
-
-const styles = StyleSheet.create({});
